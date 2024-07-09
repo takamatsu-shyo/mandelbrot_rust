683d894924ab18140557e0a683bda8c663fdc947
Sho Yano <shoyano372@gmail.com>
2024-07-09 21:56:51 +0300

    single thread

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@
 /// If a function signature has an elided lifetime parameter in return position,
 /// the error message will point the root of the problem.
 
+
 use num::Complex;
 
 /// Try to determine if 'c' is in the Mandelbrot set, using at most 'limit'
@@ -22,6 +23,7 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     }
     None
 }
+
 
 use std::str::FromStr;
 
@@ -51,6 +53,110 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<f64>("0.5x1.5",   'x'), Some((0.5, 1.5)));
 }
 
+
+/// Parse a pair of floating-point numbers
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex {re, im}),
+        None => None
+    }
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(parse_complex("1.25,-0.0625"),
+        Some(Complex { re: 1.25, im: -0.0625}));
+    assert_eq!(parse_complex(",-0.0625"), None);
+}
+
+
+/// Given the row and column of a pixel in the output image, return the
+/// corresponding point on the complex plane.
+fn pixel_to_point(bounds: (usize, usize),
+                    pixel: (usize, usize),
+                    upper_left: Complex<f64>,
+                    lower_right: Complex<f64>)
+    -> Complex<f64>
+{
+    let (width, height) = (lower_right.re - upper_left.re,
+                            upper_left.im - lower_right.im);
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 200), (25, 175),
+                                Complex {re: -1.0, im:  1.0},
+                                Complex {re:  1.0, im: -1.0}),
+            Complex { re: -0.5, im: -0.75});
+}
+
+
+/// Render a rectanble of the Mandelbrot set int to a buffer of pixels.
+fn render(pixels: &mut[u8],
+            bounds: (usize, usize),
+            upper_left: Complex<f64>,
+            lower_right: Complex<f64>)
+{
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row),
+                                        upper_left, lower_right);
+            pixels[row * bounds.0 + column] =
+                match escape_time(point, 255) {
+                    None => 0,
+                    Some(count) => 255 - count as u8
+                };
+        }
+    }
+}
+
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
+
+/// Write the buffer 'pixels', whose dimensions are given by 'bounds', to the
+/// file named 'filename'
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
+    -> Result<(), std::io::Error>
+{
+    let output = File::create(filename)?;
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels,
+                    bounds.0 as u32, bounds.1 as u32,
+                    ColorType::Gray(8))?;
+    Ok(())
+}
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} File Pixels Upper_Left Lower_Right",
+            args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]);
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds)
+        .expect("error wrinting PNG file");
 }
